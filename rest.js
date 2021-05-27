d2d168db6f1e867688da0f5ee482a25699f72de1
Saloni197 <kavita.goyal@technocodz.com>
2021-05-27 15:38:46 +0530

    todays task

--- rest.js
+++
@@ -1,8 +0,0 @@
-var display = function () {
-    for (var i in arguments) {
-        console.log(arguments[i]);
-    }
-};
-display('Red');
-display('Red', 'blue');
-display('Red', 'blue', 'green');
